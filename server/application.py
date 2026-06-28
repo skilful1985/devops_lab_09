0e926c95715f3f95b60fd688f4f9f52841b39132
Vladimir <vladimir@mylocal.ru>
2026-06-28 07:14:23 +0000

    Edit app v4

--- server/application.py
+++ server/application.py
@@ -1,23 +1,23 @@
-#Добавляем модули
+"""Добавляем модули"""
 import http.server
 import socketserver
 
 PORT = 8000
 
-# класс для демонстрации работы юнит-тестов
+""" класс для демонстрации работы юнит-тестов """
 class TestMe():
-    # функция для проверки счета
+    """ функция для проверки счета """
     def take_five(self):
         return 4
-    # функция возврата порта
+    """ функция возврата порта """
     def port(self):
         return PORT
 
 if __name__ == '__main__':
     Handler = http.server.SimpleHTTPRequestHandler
 
-# веб-сервер отвечающий на любой запрос по дефолту 
-# показывает клиенту список файлов в текущем каталоге
+""" веб-сервер отвечающий на любой запрос по дефолту
+показывает клиенту список файлов в текущем каталоге """
     with socketserver.TCPServer(("", PORT), Handler) as http:
         print("serving at port", PORT)
         http.serve_forever()
