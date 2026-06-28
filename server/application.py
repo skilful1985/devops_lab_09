5c369ec452c3a67540c08c70e6cbb0a4199a5a70
Vladimir <vladimir@mylocal.ru>
2026-06-28 06:25:40 +0000

    Edit app v2

--- server/application.py
+++ server/application.py
@@ -6,15 +6,15 @@ import socketserver
 PORT = 8000
 
 class TestMe():			# класс для демонстрации работы юнит-тестов
-  def take_five(self):
-    return 4
-  def port(self):
-    return PORT
+    def take_five(self):
+        return 4
+    def port(self):
+        return PORT
 
 if __name__ == '__main__':
-  Handler = http.server.SimpleHTTPRequestHandler
+    Handler = http.server.SimpleHTTPRequestHandler
 
 # веб-сервер отвечающий на любой запрос по дефолту показывает клиенту список файлов в текущем каталоге
-  with socketserver.TCPServer(("", PORT), Handler) as http:
-    print("serving at port", PORT)
-    http.serve_forever()
+    with socketserver.TCPServer(("", PORT), Handler) as http:
+        print("serving at port", PORT)
+        http.serve_forever()
